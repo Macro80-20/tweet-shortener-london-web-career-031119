6e34c4db9888693efc35a4fc94be8c8363487c68
fernando <fernando_lopezc@live.com>
2019-02-26 20:00:56 +0000

    Done.

---
+++ s.rb
@@ -0,0 +1,13 @@
+def bulk_tweet_shortener
+   #shortens each tweet and prints the results
+end
+
+def selective_tweet_shortener
+# shortens tweets that are more than 140 characters
+  #does not shorten tweets that are less than 130 characters
+end
+
+def shortened_tweet_truncator
+  #truncates tweets over 140 characters
+  #does not shorten tweets shorter than 140 characters
+end
--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,55 @@
 # Write your code here.
+require "pry"
+tweet = "GUISEEEEE this is so fun!
+ I'm tweeting for you guys and this tweet is SOOOO long it's
+  gonna be way more than you would think twitter can handle,
+  so shorten it up you know what I mean? I just can never tell how long to keep typing!"
+def dictionary
+  convert = {
+   "hello" => "hi",
+   "to" => "2",
+   "two" => "2",
+   "too" => "2",
+   "for" => "4",
+   "four" => "4",
+   "be" => "b",
+   "you" => "u",
+   "at" => "@",
+   "and" => "&"
+}
+end
+
+def word_substituter(tweet)
+
+  # replaces long words with their expected short form
+
+    tweet.split(" ").map do |word|
+      if dictionary.keys.include?(word.downcase)
+        word = dictionary[word.downcase]
+      else
+        word
+    end
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(tweet)
+  tweet.map do |phrase|
+    puts word_substituter(phrase)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.chars.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).chars.length > 140
+    truncated = word_substituter(tweet).chars[0..136].push("...").join("")
+  else
+    word_substituter(tweet)
+  end
+end
